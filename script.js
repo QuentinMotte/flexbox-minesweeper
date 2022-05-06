17bb17223092fd9bd405cf1610b9ae9bd1e32781
Quentin Motte <motte.quentin1@gmail.com>
2022-05-06 15:44:55 +0200

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// REPLACE THE HOVER WITH CSS -----------------
+// --------------------------------
 document.querySelectorAll(".hide").forEach((span) => {
     span.addEventListener("mouseenter", () => {
         span.classList.add("active");
@@ -6,12 +8,16 @@ document.querySelectorAll(".hide").forEach((span) => {
         span.classList.remove("active");
     });
 });
+//
+//Add a click event the boxes filled
 document.querySelectorAll(".active-ful").forEach((span) => {
     span.addEventListener("click", () => {
         span.classList.toggle("active-full");
         console.log(span);
     });
 });
+//
+//Remove the outset border for dotted border
 document.querySelectorAll(".full").forEach((span) => {
     span.addEventListener("click", () => {
         span.classList.toggle("border");
